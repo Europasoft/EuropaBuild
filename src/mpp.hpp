e70d88e69045812f1ae1bd9b90805c5f53e05318
Simon Liimatainen <47470343+therpgmaster@users.noreply.github.com>
2025-10-06 17:14:37 +0300

    Headers for including mpp features

---
+++ src/mpp.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "mpp/mir/meson/toolchains/compilers/cpp/cpp.hpp"
+#include "mpp/mir/meson/toolchains/toolchain.hpp"
+#include "mpp/mir/meson/machines.hpp"
+#include "mpp/util/log.hpp"
+#include "mpp/util/exceptions.hpp"
