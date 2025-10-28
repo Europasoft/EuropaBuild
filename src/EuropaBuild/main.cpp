ef6581df451d53dd85016dd56cc8ad65681c1683
Simon Liimatainen <47470343+therpgmaster@users.noreply.github.com>
2025-10-28 02:55:05 +0200

    Refactor core functions

--- src/EuropaBuild/main.cpp
+++ src/EuropaBuild/main.cpp
@@ -22,7 +22,54 @@ namespace EuropaBuild
 		log.log("Dependency graph \n" + _config->tree->getWholeDependecyTreeAsString(), ESLogVerbosity::Verbose, ESLog::BLUE);
 	}
 
-    std::unique_ptr<MPP::Compiler> detectCompiler()
+	int BuildTool::build()
+	{
+		try
+		{
+			// pick a compiler
+			auto compiler = detectCompiler();
+			if (not compiler)
+				throw EnvironmentException("No suitable C++ compiler found");
+			log.log("Using compiler " + compiler->id() + "\n");
+
+			log.log("Generating " + std::to_string(_config->tree->size()) + " targets");
+
+			std::shared_ptr<std::vector<TargetMapping>> targetMappings = std::make_shared<std::vector<TargetMapping>>();
+
+			size_t totalSourceFiles = 0;
+			for (const auto& target : *(_config->tree))
+			{
+				targetMappings->push_back(TargetMapping());
+				TargetMapping& mapping = targetMappings->back();
+				mapping.target = target;
+				// discover c++ source files
+				mapping.sourceFiles = discoverSourceFiles(mapping.target->sources);
+				if (mapping.sourceFiles.empty())
+					throw ConfigException("No C++ source files found for target " + target->name);
+				totalSourceFiles += mapping.sourceFiles.size();
+
+				createRelativeDirectory(target->outputPath);
+			}
+
+			log.log("Found " + std::to_string(totalSourceFiles) + " sources");
+
+			// generate ninja build file
+			generateNinjaBuild(*_config, targetMappings, compiler.get());
+
+			createRelativeDirectory(fs::current_path() / _config->intermediateDir);
+
+			log.log("Build configuration generated", ESLogVerbosity::Verbose, ESLog::CYAN);
+
+			return runNinja();
+		}
+		catch (const std::exception& e)
+		{
+			log.error("Build error: " + std::string(e.what()));
+			return 1;
+		}
+	}
+
+    std::unique_ptr<MPP::Compiler> BuildTool::detectCompiler()
     {
         const std::vector<std::string> compilers = { "g++", "clang++", "c++" };
 
@@ -38,50 +85,188 @@ namespace EuropaBuild
         return nullptr;
     }
 
-    void writeCompilerRule(std::ofstream& out, MPP::Compiler* compiler)
-    {
-        out << "rule cpp_compile" << std::endl << "  command =";
+	std::vector<fs::path> BuildTool::discoverSourceFiles(const std::vector<fs::path>& paths)
+	{
+		std::vector<fs::path> source_files;
 
-        for (const auto& cmd : compiler->command)
-        {
-            out << " " << cmd;
-        }
+		for (const auto& subdir : paths)
+		{
+			const auto relpath = fs::current_path() / subdir;
+			for (const auto& entry : fs::recursive_directory_iterator(relpath))
+			{
+				if (entry.is_regular_file())
+				{
+					const auto& path = entry.path();
+					const auto ext = path.extension();
+					if (ext == ".cpp" || ext == ".cxx" || ext == ".cc" || ext == ".c++")
+					{
+						source_files.push_back(fs::relative(path, fs::current_path()));
+					}
+				}
+			}
+		}
 
-        // add compile-only and output commands
-        auto compile_cmd = compiler->compile_only_command();
-        for (const auto& arg : compile_cmd)
-        {
-            out << " " << arg;
-        }
+		std::sort(source_files.begin(), source_files.end());
+		return source_files;
+	}
 
-        out << " ${ARGS} -o ${out} ${in}" << std::endl
-            << "  description = Compiling C++ object ${out}" << std::endl
-            << std::endl;
-    }
+	void BuildTool::generateNinjaBuild(const BuildConfig2& _config, std::shared_ptr<std::vector<TargetMapping>> mappings, MPP::Compiler* compiler)
+	{
+		std::ofstream out(fs::current_path() / "build.ninja");
 
-    void writeLinkerRule(std::ofstream& out, MPP::Compiler* compiler)
-    {
-        out << "rule cpp_link" << std::endl << "  command =";
+		out << "# EuropaBuild C++ Generated build file" << std::endl
+			<< "# ====================================" << std::endl
+			<< std::endl
+			<< "ninja_required_version = 1.8.2" << std::endl
+			<< std::endl;
 
-        for (const auto& cmd : compiler->command)
-        {
-            out << " " << cmd;
-        }
+		writeCompilerRule(out, compiler);
+		writeLinkerRule(out, compiler);
+		writeArchiverRule(out, compiler);
+		out << std::endl;
 
-        out << " ${ARGS} -o ${out} ${in}" << std::endl
-            << "  description = Linking executable ${out}" << std::endl
-            << std::endl;
-    }
+		size_t sourceFileCounter = 0;
+		std::map<std::string, std::vector<std::string>> depObjectFiles;
+		for (const TargetMapping& mapping : *mappings)
+		{
+			const auto& target = *mapping.target;
+			std::vector<std::string> objectFiles;
+			for (const fs::path& sourceFilePath : mapping.sourceFiles)
+			{
+				std::string objFile = sourceFilePathToObjFilenameString(fs::path(INTERMEDIATE_DIR), fs::path(sourceFilePath), std::to_string(sourceFileCounter));
+				sourceFileCounter++;
+				objectFiles.push_back(objFile);
+				out << "build " << objFile << ": cpp_compile " << escapeSpacesForNinja(fs::path(sourceFilePath)).string() << std::endl;
+				out << "  ARGS =" << includePathArgs(mapping) << " -std=c++20";
+				out << std::endl << std::endl;
+			}
 
-    void writeArchiverRule(std::ofstream& out, MPP::Compiler* compiler)
-    {
-        out << "rule cpp_archive" << std::endl
-            << "  command = ar rcs ${out} ${in}" << std::endl
-            << "  description = Creating static library ${out}" << std::endl
-            << std::endl;
-    }
+			const bool isExecutable = target.targetType == ETargetType::Executable;
+			const bool isDependency = target.targetType == ETargetType::Dependency;
+			const bool isStaticLib = target.targetType == ETargetType::StaticLib;
+			const bool isDynamicLib = target.targetType == ETargetType::DynamicLib;
 
-	fs::path escapeSpacesForNinja(const fs::path& p)
+			if (isDependency or isStaticLib or isDynamicLib)
+			{
+				// all targets should be in order at this point, 
+				// so if a later target depends on this one it will be able to find these object files to link against
+				depObjectFiles[target.name] = objectFiles;
+			}
+			if (isExecutable or isStaticLib or isDynamicLib)
+			{
+				// executables and libraries (archives) need to actually be linked, not just compiled
+				if (target.targetType == ETargetType::Executable)
+					out << "build " << target.name << ": cpp_link";
+				else
+					out << "build " << target.name << ": cpp_archive";
+
+				for (const std::string& obj : objectFiles)
+					out << " " << obj;
+
+				// also link against object files from dependency-targets built before this one
+				// the other target that is the dependency must be marked as such in the dependencies list for this target
+				if (target.depends.size() > 0)
+				{
+					for (const auto& depName : target.depends)
+					{
+						if (depObjectFiles.find(depName) == depObjectFiles.end())
+							throw DependencyException("Target " + target.name + " is set to depend on " + depName + " but the latter could not be found");
+
+						for (const auto& obj : depObjectFiles[depName])
+							out << " " << obj;
+					}
+				}
+				out << std::endl << std::endl;
+			}
+			if (isDynamicLib)
+			{
+				throw std::runtime_error("DLLs not yet supported");
+			}
+
+		}
+
+		// targets that no others depend on are "defaults" (final products)
+		const auto& products = _config.tree->targetsThatAreFinalProducts;
+		if (products.size() < 1)
+			throw DependencyException("Could not determine any default target because all targets are used as dependencies");
+
+		out << "default ";
+		for (const std::string& d : products)
+			out << d << " ";
+		out << std::endl;
+	}
+
+	void BuildTool::writeCompilerRule(std::ofstream& out, MPP::Compiler* compiler)
+	{
+		out << "rule cpp_compile" << std::endl << "  command =";
+
+		for (const auto& cmd : compiler->command)
+		{
+			out << " " << cmd;
+		}
+
+		// add compile-only and output commands
+		auto compile_cmd = compiler->compile_only_command();
+		for (const auto& arg : compile_cmd)
+		{
+			out << " " << arg;
+		}
+
+		out << " ${ARGS} -o ${out} ${in}" << std::endl
+			<< "  description = Compiling C++ object ${out}" << std::endl
+			<< std::endl;
+	}
+
+	void BuildTool::writeLinkerRule(std::ofstream& out, MPP::Compiler* compiler)
+	{
+		out << "rule cpp_link" << std::endl << "  command =";
+
+		for (const auto& cmd : compiler->command)
+		{
+			out << " " << cmd;
+		}
+
+		out << " ${ARGS} -o ${out} ${in}" << std::endl
+			<< "  description = Linking executable ${out}" << std::endl
+			<< std::endl;
+	}
+
+	void BuildTool::writeArchiverRule(std::ofstream& out, MPP::Compiler* compiler)
+	{
+		out << "rule cpp_archive" << std::endl
+			<< "  command = ar rcs ${out} ${in}" << std::endl
+			<< "  description = Creating static library ${out}" << std::endl
+			<< std::endl;
+	}
+
+	std::string BuildTool::sourceFilePathToObjFilenameString(const fs::path& objOutDir, const fs::path& sourcePath, std::string suffix)
+	{
+		fs::path outPath = escapeSpacesForNinja(sourcePath);
+		outPath = fs::path(outPath.stem().string() + suffix + ".o"); // add .o file extension, remove directory
+		return (objOutDir / outPath).string(); // the new path is where the generated object file goes
+	}
+
+	int8_t BuildTool::runNinja()
+	{
+		std::cout << "Running Ninja" << std::endl;
+		auto const& [ret, out, err] = MPP::process(std::vector<std::string>{ "ninja" });
+		if (ret != 0)
+		{
+			std::cerr << ESLog::colorMessage("Command failed (code " + std::to_string(ret) + ")\n" + err + "\n", ESLog::BRIGHT_RED);
+			return ret;
+		}
+		std::cout << out << std::endl;
+		std::cout << "Build completed" << std::endl;
+		return 0;
+	}
+
+	void BuildTool::createRelativeDirectory(const fs::path& path)
+	{
+		if (not fs::exists(fs::current_path() / path))
+			fs::create_directories(fs::current_path() / path);
+	}
+
+	fs::path BuildTool::escapeSpacesForNinja(const fs::path& p)
 	{
 		// escape spaces with the ninja escape character $
 		std::string str = p.string();
@@ -96,14 +281,7 @@ namespace EuropaBuild
 		return fs::path(str);
 	}
 
-    std::string sourceFilePathToObjFilenameString(const fs::path& objOutDir, const fs::path& sourcePath, std::string suffix)
-    {
-		fs::path outPath = escapeSpacesForNinja(sourcePath);
-		outPath = fs::path(outPath.stem().string() + suffix + ".o"); // add .o file extension, remove directory
-		return (objOutDir / outPath).string(); // the new path is where the generated object file goes
-    }
-
-	std::string includePathArgs(const TargetMapping& mapping)
+	std::string BuildTool::includePathArgs(const TargetMapping& mapping)
 	{
 		std::string inArgs;
 		for (const fs::path& in : mapping.target->includePaths)
@@ -113,182 +291,9 @@ namespace EuropaBuild
 		return inArgs;
 	}
 
-    void generateNinjaBuild(const BuildConfig2& _config, std::shared_ptr<std::vector<TargetMapping>> mappings, MPP::Compiler* compiler)
-    {
-        std::ofstream out(fs::current_path() / "build.ninja");
-
-        out << "# EuropaBuild C++ Generated build file" << std::endl
-            << "# ====================================" << std::endl
-            << std::endl
-            << "ninja_required_version = 1.8.2" << std::endl
-            << std::endl;
-
-        writeCompilerRule(out, compiler);
-        writeLinkerRule(out, compiler);
-        writeArchiverRule(out, compiler);
-        out << std::endl;
-
-		size_t sourceFileCounter = 0;
-        std::map<std::string, std::vector<std::string>> depObjectFiles;
-        for (const TargetMapping& mapping : *mappings)
-        {
-            const auto& target = *mapping.target;
-            std::vector<std::string> objectFiles;
-            for (const fs::path& sourceFilePath : mapping.sourceFiles)
-            {
-				std::string objFile = sourceFilePathToObjFilenameString(fs::path(INTERMEDIATE_DIR), fs::path(sourceFilePath), std::to_string(sourceFileCounter));
-				sourceFileCounter++;
-				objectFiles.push_back(objFile);
-				out << "build " << objFile << ": cpp_compile " << escapeSpacesForNinja(fs::path(sourceFilePath)).string() << std::endl;
-                out << "  ARGS =" << includePathArgs(mapping) << " -std=c++20";
-                out << std::endl << std::endl;
-            }
-
-            if (target.targetType == ETargetType::Dependency)
-            {
-                // all targets should be in order at this point, 
-                // so if a later target depends on this one it will be able to find these object files to link against
-                depObjectFiles[target.name] = objectFiles;
-            }
-            else if (target.targetType == ETargetType::Executable or target.targetType == ETargetType::StaticLib)
-            {
-                // executables and libraries (archives) need to actually be linked, not just compiled
-                if (target.targetType == ETargetType::Executable)
-                    out << "build " << target.name << ": cpp_link";
-                else
-                    out << "build " << target.name << ": cpp_archive";
-
-                for (const std::string& obj : objectFiles)
-                    out << " " << obj;
-
-                // also link against object files from dependency-targets built before this one
-                // the other target that is the dependency must be marked as such in the dependencies list for this target
-                if (target.depends.size() > 0)
-                {
-                    for (const auto& depName : target.depends)
-                    {
-                        if (depObjectFiles.find(depName) == depObjectFiles.end())
-                            throw DependencyException("Target " + target.name + " is set to depend on " + depName + " but the latter could not be found");
-
-                        for (const auto& obj : depObjectFiles[depName])
-                            out << " " << obj;
-                    }
-                }
-                out << std::endl << std::endl;
-            }
-            else if (target.targetType == ETargetType::DynamicLib)
-            {
-                throw std::runtime_error("DLLs not yet supported");
-            }
-
-        }
-
-        // targets that no others depend on are "defaults" (final products)
-        const auto& products = _config.tree->targetsThatAreFinalProducts;
-        if (products.size() < 1)
-            throw DependencyException("Could not determine any default target because all targets are used as dependencies");
-
-        out << "default ";
-        for (const std::string& d : products)
-            out << d << " ";
-        out << std::endl;
-    }
-
-    int BuildTool::build()
-    {
-        try
-        {
-            // pick a compiler
-            auto compiler = detectCompiler();
-            if (not compiler)
-                throw EnvironmentException("No suitable C++ compiler found");
-            log.log("Using compiler " + compiler->id() + "\n");
-
-            log.log("Generating " + std::to_string(_config->tree->size()) + " targets");
-
-            std::shared_ptr<std::vector<TargetMapping>> targetMappings = std::make_shared<std::vector<TargetMapping>>();
-            
-            size_t totalSourceFiles = 0;
-            for (const auto& target : *(_config->tree))
-            {
-                targetMappings->push_back(TargetMapping());
-                TargetMapping& mapping = targetMappings->back();
-                mapping.target = target;
-                // discover c++ source files
-                mapping.sourceFiles = discoverSourceFiles(mapping.target->sources);
-                if (mapping.sourceFiles.empty())
-                    throw ConfigException("No C++ source files found for target " + target->name);
-                totalSourceFiles += mapping.sourceFiles.size();
-
-				createRelativeDirectory(target->outputPath);
-            }
-
-            log.log("Found " + std::to_string(totalSourceFiles) + " sources");
-
-            // generate ninja build file
-            generateNinjaBuild(*_config, targetMappings, compiler.get());
-
-            createRelativeDirectory(fs::current_path() / _config->intermediateDir);
-
-            log.log("Build configuration generated", ESLogVerbosity::Verbose, ESLog::CYAN);
-
-            return runNinja();
-        }
-        catch (const std::exception & e)
-        {
-            log.error("Build error: " + std::string(e.what()));
-            return 1;
-        }
-    }
-
     ESLogVerbosity BuildTool::getLogVerbosity() const
     {
         return log.verbosity;
-    }
-    
-    std::vector<fs::path> BuildTool::discoverSourceFiles(const std::vector<fs::path>& paths)
-    {
-        std::vector<fs::path> source_files;
-
-        for (const auto& subdir : paths)
-        {
-			const auto relpath = fs::current_path() / subdir;
-            for (const auto& entry : fs::recursive_directory_iterator(relpath))
-            {
-                if (entry.is_regular_file()) 
-                {
-                    const auto& path = entry.path();
-                    const auto ext = path.extension();
-                    if (ext == ".cpp" || ext == ".cxx" || ext == ".cc" || ext == ".c++")
-                    {
-                        source_files.push_back(fs::relative(path, fs::current_path()));
-                    }
-                }
-            }
-        }
-
-        std::sort(source_files.begin(), source_files.end());
-        return source_files;
-    }
-    
-    int8_t BuildTool::runNinja()
-    {
-        std::cout << "Running Ninja" << std::endl;
-        auto const& [ret, out, err] = MPP::process(std::vector<std::string>{ "ninja" });
-        if (ret != 0) 
-        {
-            std::cerr << ESLog::colorMessage("Command failed (code " + std::to_string(ret) + ")\n" + err + "\n", ESLog::BRIGHT_RED);
-            return ret;
-        }
-        std::cout << out << std::endl;
-        std::cout << "Build completed" << std::endl;
-        return 0;
-    }
-
-    void BuildTool::createRelativeDirectory(const fs::path& path)
-    {
-        if (not fs::exists(fs::current_path() / path))
-            fs::create_directories(fs::current_path() / path);
     }
 
 } // namespace EuropaBuild
